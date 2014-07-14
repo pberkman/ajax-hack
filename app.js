043dc82216a4fca72ed0db5c05c4fcb7c241ef97
pberkman <patriciaberkman@Patricias-MacBook-Pro.local>
2014-07-13 17:11:16 -0700

    commit before community help

--- app.js
+++ app.js
@@ -1,12 +1,33 @@
 $(document).ready(function() {
-	// clear the gallery before a submit
-	$(".gallery-wrapper").html("");
-	$("form").sumbit(function () {
-		var hashtag=$(this).find('input[name="tags"]');
-		getPhotos(hashtag);
+	$("form").submit(function (event) {
+		event.preventDefault();
+		// clear the gallery before a submit
+		$(".gallery-wrapper").html("");
+		var hashtag=$(this).find('input[name="tags"]').val();
+		searchWorld(hashtag);
 	}); //end submit
-}); //end ready
+}); //end ready	
 
-var getPhotos = function() {
+var searchWorld = function(hashtag) {
+		
+		var getPhotos = $.ajax ({
+			url:"https://api.instagram.com/v1/media/popular?client_id=920d83e2718741b885af9d6323a498b4",
+			dataType:"jsonp",
+			type:"GET"
+		})
+		.done(function(getPhotos) {
 
-}; //end getPhotos
+			var results = $('<div class="gallery-wrapper">');
+			
+			$.each(getPhotos.data, function(index, photo) {
+				results += '<div class="imageWrapper">';
+				results += '<img src="' + photo.images + '"/>';
+				//results += '<div class="location"' + photo.location + '</div';
+				results += '</div>';
+			});
+		
+			$(".gallery-wrapper").html(results);
+		});
+};
+
+
