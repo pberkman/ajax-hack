1a17889caba92ca44f7d57a21604d3d98e7d0bdd
pberkman <patriciaberkman@Patricias-MacBook-Pro.local>
2014-07-15 21:41:53 -0700

    7-15
    photos showing but can’t filter by tags

--- app.js
+++ app.js
@@ -3,35 +3,36 @@
 		evt.preventDefault();
 		// clear the gallery before a submit
 		$(".gallery-wrapper").html("");
-		var hashtag=$(this).find('input[name="tags"]').val();
+		var hashtag=$("#search").val();
 		searchWorld(hashtag);
 	}); //end submit
 }); //end ready	
 
 var searchWorld = function(hashtag) {
 		var parameters={
-			lat:51,
-			lng:32,
+			lat:37,
+			lng:-122,
 			distance:5000
 		};
 
 		var getPhotos = $.ajax ({
-			url:"https://api.instagram.com/v1/media/search?lat=48.858844&lng=2.294351&client_id=920d83e2718741b885af9d6323a498b4",
+			url:"https://api.instagram.com/v1/media/search?client_id=920d83e2718741b885af9d6323a498b4",
 			data:parameters,
+
 			dataType:"jsonp",
 			type:"GET"
 		})
 		.done(function(getPhotos) {
 
-			var results = $('<div class="gallery-wrapper">');
+			var results='<div class="gallery-wrapper">';
 			
 			$.each(getPhotos.data, function(index, photo) {
 				results += '<div class="imageWrapper">';
-				results += '<img src="' + photo.images + '"/>';
-				//results += '<div class="location"' + photo.location + '</div';
+				results += '<img src="' + photo.images.thumbnail.url + '"/>';
+				//results += '<div class="location"' + photo.location.name + '</div';
 				results += '</div>';
 			});
-		
+			results += '</div>';
 			$(".gallery-wrapper").html(results);
 		});
 };
